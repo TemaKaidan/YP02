aa48a22354205c782e4111153f28eca7303ebcd9
student-a502 <student-a502@PC-A502-7>
2025-02-25 11:39:15 +0500

    Разработаны модели

---
+++ YP021/Models/Students.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YP021.Models
+{
+    public class Students
+    {
+        public int id { get; set; }
+        public string surname { get; set; }
+        public string name { get; set; }
+        public string lastname { get; set; }
+        public int studGroupId { get; set; }
+        public DateTime dateOfRemand { get; set; }
+        public int userId { get; set; }
+    }
+}
