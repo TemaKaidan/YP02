c8478a0f4020ffac314213236d44fbdd49454534
student-a502 <student-a502@PC-A502-7>
2025-02-25 11:51:48 +0500

    Разработаны Main + Student

--- YP021/Pages/Main.xaml.cs
+++ YP021/Pages/Main.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -20,9 +21,65 @@ namespace YP021.Pages
     /// </summary>
     public partial class Main : Page
     {
+        private static Main _instance;
+
+        private bool isMenuCollapsed = false;
+
+        public static Main Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new Main();
+                }
+                return _instance;
+            }
+        }
+
         public Main()
         {
             InitializeComponent();
+        }
+
+        private void ToggleMenu(object sender, RoutedEventArgs e)
+        {
+            DoubleAnimation widthAnimation = new DoubleAnimation();
+
+            if (isMenuCollapsed)
+            {
+                widthAnimation.From = 50;
+                widthAnimation.To = 200;
+                MenuPanel.Width = 200;
+                foreach (UIElement element in MenuPanel.Children)
+                {
+                    if (element is Button btn && btn.Content.ToString() != "☰")
+                    {
+                        btn.Visibility = Visibility.Visible;
+                    }
+                }
+            }
+            else
+            {
+                widthAnimation.From = 200;
+                widthAnimation.To = 50;
+                foreach (UIElement element in MenuPanel.Children)
+                {
+                    if (element is Button btn && btn.Content.ToString() != "☰")
+                    {
+                        btn.Visibility = Visibility.Collapsed;
+                    }
+                }
+            }
+
+            widthAnimation.Duration = new Duration(TimeSpan.FromSeconds(0.3));
+            MenuPanel.BeginAnimation(WidthProperty, widthAnimation);
+            isMenuCollapsed = !isMenuCollapsed;
+        }
+
+        private void Click_Student(object sender, RoutedEventArgs e)
+        {
+            MainWindow.init.OpenPages(MainWindow.pages.student);
         }
     }
 }
